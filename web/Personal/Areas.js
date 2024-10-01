e057b9da5431c3340e0e1f1d9c81b0e1a4a078d5
Sergio Vázquez <138184485+SergioVazRizo@users.noreply.github.com>
2024-10-01 15:15:42 -0600

    Cambios Diseño y barra de navegacion de Cortez

--- web/Personal/Areas.js
+++ web/Personal/Areas.js
@@ -1,3 +1,73 @@
+// Barre de navegacion
+function cargarNavegacion() {
+const rol = localStorage.getItem('rol'); // Obtener el rol del usuario desde localStorage
+        const navbarContainer = document.getElementById('navbar'); // Asumiendo que tienes un div con id "navbar" en tu HTML
+
+        if (rol === 'ESTUDIANTE') {
+navbarContainer.innerHTML = `
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="PrincipalPrueba.html">Soy estudiante</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav me-auto mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="PrincipalPrueba.html">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Becas</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="becas/CatalogoBecas.html">Registrarse</a></li>
+                                    <li><a class="dropdown-item" href="becas/ResultadosBecas.html">Resultados</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        `;
+        } else if (rol === 'ESCOLAR') {
+navbarContainer.innerHTML = `
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="PrincipalPrueba.html">Servicios Escolares</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav me-auto mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="PrincipalPrueba.html">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Becas</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="becas/validarSolicitud.html">Validar Solicitudes</a></li>
+                                    <li><a class="dropdown-item" href="becas/ResultadosBecas.html">Resultados</a></li>
+                                    <li><a class="dropdown-item" href="becas/agregarBecas.html">Nuevas Becas</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        `;
+        } 
+}
+
+window.onload = cargarNavegacion();
+
+
 const apiBaseUrl = 'http://localhost:8084/api'; // Cambia según tu backend
 
 // Cargar áreas en el formulario de empleados y la tabla de áreas
